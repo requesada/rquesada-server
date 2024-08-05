4eb738905be4441d90969bb89063995f2790d76d
Roberto Quesada <rquesada@protonmail.ch>
2024-08-05 10:02:11 -0700

    Add environment var for port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const serverApp = express()
-const port = 3000
+const port = process.env.PORT
 const directoryName = process.cwd()
 
 serverApp.use(express.static(path.join(directoryName, 'dist')))
